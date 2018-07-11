cd9be9dba59cc45e8e806043929417a16d7487f8
Sivadass N <nsivadass@gmail.com>
2018-07-11 23:59:56 +0530

    Added header

--- client-src/partials/header.js
+++ client-src/partials/header.js
@@ -3,7 +3,7 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Job Board</h1>
       </div>
     );
